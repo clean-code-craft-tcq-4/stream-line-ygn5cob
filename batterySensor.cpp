2731816b51b8bcdb0a7c4b47ce0e5295f41e8200
Gnanasekaran Yuvaraja (MS/ECR4-XC) <ygn5cob@bosch.com>
2022-09-07 17:50:18 +0530

    commit-3

--- batterySensor.cpp
+++ batterySensor.cpp
@@ -4,6 +4,24 @@
 #include <assert.h>
 using namespace std;
 
+void EbatterySensorData(int sensor1[],int sensor2[]) {
+    
+    int index, buffer_index = 0;
+    for(index = 0; index < NUMBER_OF_SAMPLES; index++) {
+
+        cout << left << "  sensor1 : " << batterysensor1[index]
+            << left << "  sensor2 : " << batterysensor2[index]
+            << std::endl;
+
+        EbatterysensorOutput[buffer_index]
+                << left << "sensor1 : " << batterysensor1[index]
+                << left << "sensor2 : " << batterysensor2[index]
+                << std::endl;
+
+        ++buffer_index;
+    }
+}
+
 void AbatterySensorData(int sensor1[],int sensor2[]) {
     
     int index, buffer_index = 0;
@@ -23,26 +41,10 @@ void AbatterySensorData(int sensor1[],int sensor2[]) {
 }
 
 bool TestSensorData(int sensor1[],int sensor2[]) {
-
-    int index, buffer_index = 0;
-    for(index = 0; index < NUMBER_OF_SAMPLES; index++) {
-
-        cout << left << "  sensor1 : " << batterysensor1[index]
-            << left << "  sensor2 : " << batterysensor2[index]
-            << std::endl;
-
-        EbatterysensorOutput[buffer_index]
-                << left << "sensor1 : " << batterysensor1[index]
-                << left << "sensor2 : " << batterysensor2[index]
-                << std::endl;
-        
+   
         if(AbatterysensorOut[index].str() != EbatterysensorOutput[index].str()) {
             return false;
             break;
         }
-
-        ++buffer_index;
-    }
-
     return true;
 }
