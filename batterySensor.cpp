acaa79b4aa28846949f3a001e6236e793a92d100
Gnanasekaran Yuvaraja (MS/ECR4-XC) <ygn5cob@bosch.com>
2022-09-07 19:27:17 +0530

    change-8

--- batterySensor.cpp
+++ batterySensor.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 const int NUMBER_OF_SAMPLES = 50;
-std::stringstream AbatterysensorOut[NUMBER_OF_SAMPLES], EbatterysensorOutput[NUMBER_OF_SAMPLES];
+std::stringstream AbatterysensorOut[NUMBER_OF_SAMPLES], EbatterysensorOut[NUMBER_OF_SAMPLES];
 
 void EbatterySensorData(int sensor1[],int sensor2[]) {
     
@@ -16,7 +16,7 @@ void EbatterySensorData(int sensor1[],int sensor2[]) {
             << left << "  sensor2 : " << batterysensor2[index]
             << std::endl;
 
-        EbatterysensorOutput[buffer_index] 
+        EbatterysensorOut[buffer_index] 
             << left << "sensor1 : " << batterysensor1[index] 
             << left << "sensor2 : " << batterysensor2[index] 
             << std::endl;
@@ -34,7 +34,7 @@ void AbatterySensorData(int sensor1[],int sensor2[]) {
             << left << "  sensor2 : " << batterysensor2[index]
             << std::endl;
 
-        AbatterysensorOutput[buffer_index]
+        AbatterysensorOut[buffer_index]
             << left << "sensor1 : " << batterysensor1[index]
             << left << "sensor1 : " << batterysensor2[index]
             << std::endl;
@@ -47,7 +47,7 @@ bool TestSensorData(int sensor1[],int sensor2[]) {
    
     EbatterySensorData(sensor1, sensor2);
     for(int  index= 0; index < NUMBER_OF_SAMPLES; index++){
-        if(AbatterysensorOutput[index].str() != EbatterysensorOutput[index].str()) {
+        if(AbatterysensorOut[index].str() != EbatterysensorOut[index].str()) {
             return false;
             break;
         }
