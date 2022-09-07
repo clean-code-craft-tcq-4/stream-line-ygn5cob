0428001dd5b7b9a29b0e338cbc8eec73903d00f1
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-09-07 21:59:06 +0530

    Update batterySensor.cpp

--- batterySensor.cpp
+++ batterySensor.cpp
@@ -4,11 +4,9 @@
 #include <assert.h>
 using namespace std;
 
-list<string> AbatterysensorOut, EbatterysensorOut;
-
 bool TestSensorData(list<string>& sensor1,list<string>& sensor2) {
    
-    EbatterySensorData(sensor1, sensor2);
+    batterySensorData(sensor1, sensor2,EbatterysensorOut);
 for(list<string>::iterator itr=AbatterysensorOut.begin();itr!=AbatterysensorOut.end(); ++itr)
  {
    for(list<string>::iterator it2 = EbatterysensorOut.begin(); it2!=EbatterysensorOut.end(); ++it2)
@@ -23,7 +21,7 @@ for(list<string>::iterator itr=AbatterysensorOut.begin();itr!=AbatterysensorOut.
     return true;
 }
 
-void EbatterySensorData(list<string>& sensor1,list<string>& sensor2) {
+batterySensorData(list<string>& sensor1,list<string>& sensor2,list<string>& output){
    
     int index, buffer_index = 0;
     for(list<string>::iterator itr=sensor1.begin();itr!=sensor1.end(); ++itr)
@@ -31,20 +29,9 @@ void EbatterySensorData(list<string>& sensor1,list<string>& sensor2) {
         for(list<string>::iterator it2 = sensor2.begin(); it2!=sensor2.end(); ++it2)
         {
             string sensor_output = *itr + ":" + *it2;
-            EbatterysensorOut.push_back(sensor_output);
+            output.push_back(sensor_output);
         }
     }
 }
 
-void AbatterySensorData(list<string>& sensor1,list<string>& sensor2) {
-       
-    int index, buffer_index = 0;
-    for(list<string>::iterator itr=sensor1.begin();itr!=sensor1.end(); ++itr)
-    {
-        for(list<string>::iterator it2 = sensor2.begin(); it2!=sensor2.end(); ++it2)
-        {
-            string sensor_output = *itr + ":" + *it2;
-            AbatterysensorOut.push_back(sensor_output);
-        }
-    }
 }
