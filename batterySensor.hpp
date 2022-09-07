c678f295670284153ea5dd6bbb82fc6138e72192
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-09-07 22:14:01 +0530

    Update batterySensor.hpp

--- batterySensor.hpp
+++ batterySensor.hpp
@@ -10,6 +10,7 @@
 #include <list>
 using namespace std;
 
+extern list<string> AbatterysensorOut, EbatterysensorOut;
 bool TestSensorData(list<string>& sensor1,list<string>& sensor2);
 void batterySensorData(list<string>& sensor1,list<string>& sensor2,list<string>& output);
 //void EbatterySensorData(list<string>& sensor1, list<string>& sensor2);
