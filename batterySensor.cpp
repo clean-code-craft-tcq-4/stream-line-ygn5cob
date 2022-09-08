88a3235b7c20e0fae3ba94058e18c50ea13533ec
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-09-08 19:38:53 +0530

    Update batterySensor.cpp

--- batterySensor.cpp
+++ batterySensor.cpp
@@ -9,7 +9,7 @@ bool TestSensorData(list<string>& sensor1,list<string>& sensor2) {
       
     batterySensorData(sensor1, sensor2,EbatterysensorOut);
     list<string>::iterator itr = AbatterysensorOut.begin();
-    list<string>::iterator it2 = EbatterysensorOut.begin()
+    list<string>::iterator it2 = EbatterysensorOut.begin();
       
 for(;itr!=AbatterysensorOut.end(),it2!=EbatterysensorOut.end(); ++itr,++it2)
  {
@@ -27,7 +27,7 @@ void batterySensorData(list<string>& sensor1,list<string>& sensor2,list<string>&
     int index, buffer_index = 0;
      cout<<"sensor ouput List:"<<"\n";
     list<string>::iterator itr = sensor1.begin();
-    list<string>::iterator it2 = sensor2.begin()
+    list<string>::iterator it2 = sensor2.begin();
       
     for(;itr!=sensor1.end(),it2!=sensor2.end(); ++itr,++it2)
     {
